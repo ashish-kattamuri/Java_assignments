a0bddca0b5aeb0fcf0899001138e09b9774e0a9b
ashish-kattamuri <69315739+ashish-kattamuri@users.noreply.github.com>
2022-01-23 12:39:23 +0530

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,18 +1,18 @@
-package yourname.assingment.main;
-import yourname.assignment.data.A;
-import yourname.assignment.singleton.B;
+package Ashish.assingment.main;
+import Ashish.assignment.data.first;
+import Ashish.assignment.singleton.second;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		A obj =new A();
+		first obj =new first();
 		//global variables which can get default values.
 		obj.disp();
 		//local variables which are not initialized but does not get any default values.
 		obj.show();
 		//static methods can only access static variables.
-		B obj1 = new B("str");
-		obj1.B_method("str");
+		second obj1 = new second("str");
+		obj1.second_method("str");
 		obj1.print();
 		
 	}
